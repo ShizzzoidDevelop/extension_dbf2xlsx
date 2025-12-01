337d9f57a5ac2d270f1639b35c361a97872f6328
Тима Таскин <timataskin@172-1-1-200.lightspeed.hstntx.sbcglobal.net>
2025-12-01 14:51:02 +0700

    2.2.1

---
+++ Dbf2XlsxConverter.CrossPlat/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Dbf2XlsxConverter.CrossPlat;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
